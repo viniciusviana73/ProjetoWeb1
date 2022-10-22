59441d507b5f88d5d9e56316abd90136deb0f17c
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2022-10-22 13:01:11 -0300

    api classify text encrypt

---
+++ projeto/script/encrypt.js
@@ -0,0 +1,52 @@
+let textAreaEncrypt = document.querySelector('#txtEncrypt')
+let inputKeyC       = document.querySelector('#inputKeyC')
+let divEncrypt      = document.querySelector('#divCripto')
+let menuEncrypt     = document.querySelector('#targetEncrypt')
+let textAreaDecrypt = document.querySelector('#txtDecrypt')
+let inputKeyU       = document.querySelector('#inputKeyU')
+let divDecrypt      = document.querySelector('#divUncripto')
+let resultado       = document.querySelector('#resultado')
+
+menuEncrypt.click()
+
+async function encryptData() {
+    try {
+        const url      = 'https://classify-web.herokuapp.com/api/encrypt';
+        const jsonData = JSON.stringify({ 
+            data: String(textAreaEncrypt.value), 
+            key:  String(inputKeyC.value)
+        });
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: jsonData
+        });
+        const result = await response.json();
+        textAreaEncrypt.value = `${result.result}`
+    } catch (error) {
+        alert(`Erro: ${error}`);
+    }
+}
+
+async function decryptData(){
+    try {
+        const url      = 'https://classify-web.herokuapp.com/api/decrypt';
+        const jsonData = JSON.stringify({ 
+            data: String(textAreaDecrypt.value), 
+            key:  String(inputKeyU.value)
+        });
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: jsonData
+        });
+        const result = await response.json();
+        resultado.innerHTML = ` <p>Resultado texto descriptografado com a chave "${String(inputKeyU.value)}": ${JSON.stringify(result.result)}</p>`
+    } catch (error) {
+        alert(`Erro: ${error}`);
+    }
+}
