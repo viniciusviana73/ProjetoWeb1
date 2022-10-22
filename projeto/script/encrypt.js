b456f61a6ba7e6e09cfe745b524e96c24726e66e
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2022-10-22 15:10:39 -0300

    validação inputs

--- projeto/script/encrypt.js
+++ projeto/script/encrypt.js
@@ -10,43 +10,58 @@ let resultado       = document.querySelector('#resultado')
 menuEncrypt.click()
 
 async function encryptData() {
-    try {
-        const url      = 'https://classify-web.herokuapp.com/api/encrypt';
-        const jsonData = JSON.stringify({ 
-            data: String(textAreaEncrypt.value), 
-            key:  String(inputKeyC.value)
-        });
-        let response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: jsonData
-        });
-        const result = await response.json();
-        textAreaEncrypt.value = `${result.result}`
-    } catch (error) {
-        alert(`Erro: ${error}`);
+    if (checkInput(textAreaEncrypt, inputKeyC)){
+        try {
+            const url      = 'https://classify-web.herokuapp.com/api/encrypt';
+            const jsonData = JSON.stringify({ 
+                data: String(textAreaEncrypt.value), 
+                key:  String(inputKeyC.value)
+            });
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: jsonData
+            });
+            const result = await response.json();
+            textAreaEncrypt.value = `${result.result}`
+        } catch (error) {
+            alert(`Erro: ${error}`);
+        }
     }
 }
 
 async function decryptData(){
-    try {
-        const url      = 'https://classify-web.herokuapp.com/api/decrypt';
-        const jsonData = JSON.stringify({ 
-            data: String(textAreaDecrypt.value), 
-            key:  String(inputKeyU.value)
-        });
-        let response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: jsonData
-        });
-        const result = await response.json();
-        resultado.innerHTML = ` <p>Resultado texto descriptografado com a chave "${String(inputKeyU.value)}": ${JSON.stringify(result.result)}</p>`
-    } catch (error) {
-        alert(`Erro: ${error}`);
+    if (checkInput(textAreaDecrypt, inputKeyU)){
+        try {
+            const url      = 'https://classify-web.herokuapp.com/api/decrypt';
+            const jsonData = JSON.stringify({ 
+                data: String(textAreaDecrypt.value), 
+                key:  String(inputKeyU.value)
+            });
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: jsonData
+            });
+            const result = await response.json();
+            resultado.innerHTML = ` <p>Resultado texto descriptografado com a chave "${String(inputKeyU.value)}": ${JSON.stringify(result.result)}</p>`
+        } catch (error) {
+            alert(`Erro: ${error}`);
+        }
+    }
+}
+
+function checkInput(textArea, inputKey){
+    if (textArea.value == "" || inputKey.value == ""){
+        resultado.innerHTML   = `<p>Erro: Preencha todos os campos corretamente!</p>`
+        resultado.style.color = 'red'
+        return false
+    } else {
+        resultado.style.color = 'white'
+        return true
     }
 }
