d0aacc705eacfd366ca84d9ee3d884e4e79acf25
Rafael Gonçalves <112734899+ra-goncalves@users.noreply.github.com>
2022-10-28 20:35:55 -0300

    add fetch

--- projeto/script/login.js
+++ projeto/script/login.js
@@ -81,3 +81,17 @@ function hideLogin(){
 function showLogin(){
   sectionLogin.classList.add('open')
 }
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  fetch("https://reqres.in/api/login", {
+      method: 'POST',
+      headers : {"Content-Type": "application/json"} ,
+      body: JSON.stringify({
+          "email":"eve.holt@reqres.in",
+          "password":"cityslicka"
+      })
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+})
