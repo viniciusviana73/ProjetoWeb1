28b9deb7fbe96db4dfc78d6eb7e4cf9e12a08861
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2022-12-16 11:00:31 -0300

    fix model Noticias

--- server/models/Noticias.js
+++ server/models/Noticias.js
@@ -7,39 +7,48 @@ const dbName = "Projeto_Web"
 
 module.exports = class Noticias {
     static async find(termo) {
-        await client.connect()
-        const db = client.db(dbName),
-              col = db.collection("Noticias")
-        let result
-        
-        if (termo) {
-            result = await db.collection('Noticias')
-                                .find({ $or :
-                                        [{ title : { '$regex' : termo, '$options' : 'i' }},
-                                         { content : { '$regex' : termo, '$options' : 'i' }}]
-                                      }).toArray() 
-        } else {
-            result = await db.collection('Noticias').find().toArray()
-        }
-        await client.close();
-        return result
+        try {
+            await client.connect()
+            const db = client.db(dbName),
+                  col = db.collection("Noticias")
+            let result
+            
+            if (termo) {
+                result = await col.find({ $or :
+                                            [{ title : { '$regex' : termo, '$options' : 'i' }},
+                                             { content : { '$regex' : termo, '$options' : 'i' }}]
+                                        }).toArray() 
+            } else {
+                result = await col.find().toArray()
+            }
+            return result
+        } catch (error) {
+            console.log(`Erro Noticias.find() -> ${error}`)
+        } finally {
+            await client.close()
+        }        
     }
 
     static async insert(title, content, image) {
         if (title && content && image){
-            await client.connect()
-            const db = client.db(dbName),
-                  col = db.collection("Noticias")
-            let date = new Date().toLocaleString("pt-br")
+            try {
+                await client.connect()
+                const db = client.db(dbName),
+                      col = db.collection("Noticias")
+                let date = new Date().toLocaleString("pt-br")
             
-            await col.insertOne({
-                                title: title,
-                                content: content,
-                                image: image,
-                                pTime: date
-                                })
-            await client.close();
-            console.log('Matéria cadastrada!')
+                await col.insertOne({
+                                    title: title,
+                                    content: content,
+                                    image: image,
+                                    pTime: date
+                                    })
+                console.log('Matéria cadastrada!')
+            } catch (error) {
+                console.log(`Erro Noticias.insert() -> ${error}`)
+            } finally {
+                await client.close()
+            }            
         } else {
             console.log('Preencha todos os campos.')
         }
